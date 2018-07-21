b55dce47af714aefd26ffa358ac0dd8d6a9ae954
Hugo Montes <hugomontes927@gmail.com>
2018-07-21 13:24:04 -0400

    Inicio del proyecto

---
+++ src/main/java/com/montes/springbase/SpringbaseApplication.java
@@ -0,0 +1,12 @@
+package com.montes.springbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbaseApplication.class, args);
+	}
+}
